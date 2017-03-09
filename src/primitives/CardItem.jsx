49ba051b05b360a0b0782d0b59d129b95e9959c3
niksmac <hello@nikhilmohan.in>
2017-03-09 13:47:54 +0530

    Implementing Card UI

--- src/primitives/CardItem.jsx
+++ src/primitives/CardItem.jsx
@@ -3,25 +3,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CardItem = styled.button`
-  display: inline-block;
-  border: 1px solid ${props => props.theme.ansBorder};
-  padding: .5rem 1rem;
-  font-weight: 200;
-  color: #222;
-  font-size: .8rem;
-  background: none;
-  border-radius: 1.5rem;
-  transition: background 100ms;
-  cursor: pointer;
-  margin-right: .5rem;
-  margin-bottom: 1rem;
-
-  &:focus,
-  &:hover {
-    outline: 0;
-    background: ${props => props.theme.userBlobColor};
-    color: #111;
+const CardItem = styled.div`
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 1px 1px 8px 0 rgba(0,0,0,0.1);
+  
+  .card-outer {
+    display: inline-block;
+    display: flex;
+    width: 33%;
+    padding: 0 15px;
+    margin: 0 0 30px;
+    vertical-align: top;
+  }
+  .card {
+    background-color: white;
+    border-radius: 4px;
+    box-shadow: 1px 1px 8px 0 rgba(0,0,0,0.1);
+  }
+  .card__photo {
+    background-color: #B1ECD8;
+    height: 120px;
+  }
+  .card__inner {
+    padding: 15px;
+  }
+  .card__content {
+    height: 15px;
+    background-color: #B1ECD8;
+    margin-bottom: 10px;
+  }
+  .card__content--short {
+    width: 75%;
   }
 `;
 
