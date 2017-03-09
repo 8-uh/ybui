f71d5e57f540cadbd07dfee2191f2359b9a1f0ca
niksmac <hello@nikhilmohan.in>
2017-03-09 15:24:45 +0530

    Added Flexbox EQW cards

--- src/global-styles.js
+++ src/global-styles.js
@@ -14,4 +14,18 @@ injectGlobal`
     body {
       margin: 0;
     }
+    .iconset {
+      display: flex;
+
+      span {
+        flex: 1;
+  width: 0;
+  margin-right: 10px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+      }
+
+    }
 `;
