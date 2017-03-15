6a10ee56651201ca4e527cf3a676f75d3b9f108f
niksmac <hello@nikhilmohan.in>
2017-03-15 09:31:17 +0530

    Fixed theme variable invocation

--- src/global-styles.js
+++ src/global-styles.js
@@ -7,8 +7,6 @@
 import { injectGlobal } from 'styled-components';
 import theme from './theme'
 
-
-
 injectGlobal`
     *, *:before, *:after {
       box-sizing: border-box;
@@ -16,7 +14,11 @@ injectGlobal`
 
     body {
       margin: 0;
+      margin-top: 20px;
       font-weight: 200;
+    }
+    .draggable {
+      -webkit-app-region: drag;
     }
     .userInputForm {
       width: 100%;
@@ -26,11 +28,33 @@ injectGlobal`
     }
     ul.answerli {
       padding-left: 10px;
+      margin: 0;
     }
     .spDivider {
       clear: both;
       display: block;
       margin: 15px 0;
-      color: ${props => props.theme.whiteLine};
+      color: ${theme.whiteLine};
     }
+    .imgbrowser {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-radius: 5px;
+      padding: 5px;
+      min-height: 40vh;
+      border-style: dashed;
+      cursor: pointer;
+      border-width: 2px;
+      border-color: ${theme.whiteLine};
+    }
+    .activiImgbrowser {
+      border-width: 3px;
+      border-color: ${theme.ansColor};
+    }
+    .material-icons.md-18 { font-size: 18px; }
+    .material-icons.md-24 { font-size: 24px; }
+    .material-icons.md-36 { font-size: 36px; }
+    .material-icons.md-48 { font-size: 48px; }
 `;
